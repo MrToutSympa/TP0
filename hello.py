2a005acc2f41f5c3b0af8316e6520f8a4df10e0d
Radwan Damdami <2053777@etu.cegep-lanaudiere.qc.ca>
2024-09-05 19:46:40 -0400

    moiradwan

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
+a = math.pi X rayon ** 2
+print(a)
+
+#TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+
+Radwan Damdami
 
 
